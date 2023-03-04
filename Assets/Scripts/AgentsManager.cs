9426e7514352ea8a040a790ac32ec27f545be917
Pawel <>
2023-03-04 13:22:56 +0100

    spawn agent added

---
+++ Assets/Scripts/AgentsManager.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AgentsManager : MonoBehaviour
+{
+    public Action<int> onAgentsNumChanged;
+
+    public GameObject agentPrefab;
+    public Transform ground;
+    
+    List<GameObject> allAgents = new List<GameObject>();
+
+    float planeSize = 5f;
+    float groundSizeX;
+    float groundSizeZ;
+
+    void Awake()
+    {
+        groundSizeX = ground.lossyScale.x * planeSize;
+        groundSizeZ = ground.lossyScale.z * planeSize;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnAgent();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            SpawnAgent(10);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            DestroyAgent();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetAgents();
+        }
+    }
+
+    public void SpawnAgent()
+    {
+        GameObject newAgent = Instantiate(agentPrefab);
+        allAgents.Add(newAgent);
+
+        newAgent.transform.position = new Vector3(
+            Random.Range(-groundSizeX, groundSizeX),
+            0f,
+            Random.Range(-groundSizeZ, groundSizeZ)
+        );
+        
+        if (onAgentsNumChanged != null)
+        {
+            onAgentsNumChanged(allAgents.Count);
+        }
+    }
+
+    public void SpawnAgent(int spawnNum)
+    {
+        for (int i = 0; i < spawnNum; i++)
+        {
+            SpawnAgent();
+        }
+    }
+
+    public void DestroyAgent()
+    {
+        if (allAgents.Count == 0) return;
+        
+        GameObject destroyAgent = allAgents[allAgents.Count - 1];
+        allAgents.RemoveAt(allAgents.Count - 1);
+        Destroy(destroyAgent);
+
+        if (onAgentsNumChanged != null)
+        {
+            onAgentsNumChanged(allAgents.Count);
+        }
+    }
+
+    public void ResetAgents()
+    {
+        for (int i = 0; i < allAgents.Count; i++)
+        {
+            Destroy(allAgents[i]);
+        }
+        
+        allAgents.Clear();
+        
+        if (onAgentsNumChanged != null)
+        {
+            onAgentsNumChanged(allAgents.Count);
+        }
+    }
+}
--- Assets/Scripts/UI/SpawnerUI.cs
+++ Assets/Scripts/UI/SpawnerUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -5,23 +6,39 @@ using UnityEngine;
 
 public class SpawnerUI : MonoBehaviour
 {
+    public AgentsManager agentsManager;
     public TMP_Text agentsNumText;
     
     int agentsNum;
 
+    void Awake()
+    {
+        agentsManager.onAgentsNumChanged += AgentsNumChanged;
+    }
+
+    void OnDestroy()
+    {
+        agentsManager.onAgentsNumChanged -= AgentsNumChanged;
+    }
+
     public void SpawnAgent()
     {
-        
+        agentsManager.SpawnAgent();
+    }
+    
+    public void SpawnAgents(int spawnNum)
+    {
+        agentsManager.SpawnAgent(spawnNum);
     }
 
     public void DestroyAgent()
     {
-        
+        agentsManager.DestroyAgent();
     }
 
     public void ResetAgents()
     {
-      
+        agentsManager.ResetAgents();
     }
 
     public void AgentsNumChanged(int setAgentNum)
